f0a19aa466faabaeb02d469bffecf6d184427951
Josh Gunderson <joshg253@users.noreply.github.com>
2018-07-22 21:55:38 -0700

    completed Part 7

--- death_functions.py
+++ death_functions.py
@@ -5,6 +5,7 @@ sys.path.append(libtcod_dir + 'python/')
 
 import libtcodpy as libtcod
 
+from game_messages import Message
 from game_states import GameStates
 from render_functions import RenderOrder
 
@@ -13,11 +14,12 @@ def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
 
-    return 'You died!', GameStates.PLAYER_DEAD
+    return Message('You died!', libtcod.red), GameStates.PLAYER_DEAD
 
 
 def kill_monster(monster):
-    death_message = '{0} is dead!'.format(monster.name.capitalize())
+    death_message = Message('{0} is dead!'.format(
+        monster.name.capitalize()), libtcod.orange)
 
     monster.char = '%'
     monster.color = libtcod.dark_red
